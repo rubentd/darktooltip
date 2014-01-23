3c4a7d2cd0b15a8a6830796f520162bb6c67b11b
Yapo:Ruben Torres <ruben@schibsted.cl>
2014-01-23 10:11:28 -0300

    Updated version number

--- js/jquery.darktooltip.js
+++ js/jquery.darktooltip.js
@@ -1,5 +1,5 @@
 /* 
- * DarkTooltip v0.1.3
+ * DarkTooltip v0.1.4
  * Simple customizable tooltip with confirm option and 3d effects
  * (c)2014 Rubén Torres - rubentdlh@gmail.com
  * Released under the MIT license
