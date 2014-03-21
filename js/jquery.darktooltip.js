dd5056a00a1edfe515613e18af7c553e3bec7ae9
Yapo:Rubn Torres <ruben@schibsted.cl>
2014-03-21 17:02:34 -0300

    Fix relative-positioning issue by refactoring mouseover behavior

--- js/jquery.darktooltip.js
+++ js/jquery.darktooltip.js
@@ -1,5 +1,5 @@
 /* 
- * DarkTooltip v0.1.6
+ * DarkTooltip v0.1.7
  * Simple customizable tooltip with confirm option and 3d effects
  * (c)2014 Rubén Torres - rubentdlh@gmail.com
  * Released under the MIT license
@@ -10,19 +10,32 @@
 	function DarkTooltip(element, options){
 		this.bearer = element;
 		this.options = options;
-		this.delay;
+		this.hideEvent;
+		this.mouseOverMode=(this.options.trigger == "hover" || this.options.trigger == "mouseover" || this.options.trigger == "onmouseover");
 	}
 
 	DarkTooltip.prototype = {
 		show: function(){
+			var dt = this;
 			//Close all other tooltips
-			$('ins.dark-tooltip').hide();
-			window.clearTimeout(this.delay);
 			this.tooltip.css('display', 'block');
+			//Set event to prevent tooltip from closig when mouse is over the tooltip
+			if(dt.mouseOverMode){
+				this.tooltip.mouseover( function(){
+					clearTimeout(dt.hideEvent);
+				});
+				this.tooltip.mouseout( function(){
+					clearTimeout(dt.hideEvent);
+					dt.hide();
+				});
+			}
 		},
 
 		hide: function(){
-			this.tooltip.hide();
+			var dt=this;
+			this.hideEvent = setTimeout( function(){
+				dt.tooltip.hide();
+			}, 100);
 		},
 
 		toggle: function(){
@@ -56,6 +69,7 @@
 			}else if(this.bearer.attr("data-tooltip")){
 				this.content = this.bearer.attr("data-tooltip");
 			}else{
+				// console.log("No content for tooltip: " + this.bearer.selector);
 				return;
 			}
 			if(this.content.charAt(0) == '#'){
@@ -70,7 +84,8 @@
 				+ this.options.gravity + "'><div>" + this.content + "</div><div class = 'tip'></div></ins>");
 			this.tip = this.tooltip.find(".tip");
 			
-			$(this.bearer).append(this.tooltip);
+			$("body").append(this.tooltip);
+			
 			//Adjust size for html tooltip
 			if(this.contentType == 'html'){
 				this.tooltip.css('max-width','none');
@@ -83,30 +98,25 @@
 		},
 
 		setPositions: function(){
-			var leftPos = 0;
-			var topPos = 0;
-			var bearerTop = this.bearer.offset().top;
-			var bearerLeft = this.bearer.offset().left;
-			if(this.bearer.css('position')=='fixed' || this.bearer.css('position')=='absolute'){
-				bearerTop=0;
-				bearerLeft=0;
-			}
+			var leftPos = this.bearer.offset().left;
+			var topPos = this.bearer.offset().top;
+
 			switch(this.options.gravity){
 				case 'south':
-					leftPos = bearerLeft + this.bearer.outerWidth()/2 - this.tooltip.outerWidth()/2;
-					topPos = bearerTop - this.tooltip.outerHeight() - this.tip.outerHeight()/2;
+					leftPos += this.bearer.outerWidth()/2 - this.tooltip.outerWidth()/2;
+					topPos += -this.tooltip.outerHeight() - this.tip.outerHeight()/2;
 					break;
 				case 'west':
-					leftPos = bearerLeft + this.bearer.outerWidth() + this.tip.outerWidth()/2;
-					topPos = bearerTop + this.bearer.outerHeight()/2 - (this.tooltip.outerHeight()/2);
+					leftPos += this.bearer.outerWidth() + this.tip.outerWidth()/2;
+					topPos += this.bearer.outerHeight()/2 - (this.tooltip.outerHeight()/2);
 					break;
 				case 'north':
-					leftPos = bearerLeft + this.bearer.outerWidth()/2 - (this.tooltip.outerWidth()/2);
-					topPos = bearerTop + this.bearer.outerHeight() + this.tip.outerHeight()/2;
+					leftPos += this.bearer.outerWidth()/2 - (this.tooltip.outerWidth()/2);
+					topPos += this.bearer.outerHeight() + this.tip.outerHeight()/2;
 					break;
 				case 'east':
-					leftPos = bearerLeft - this.tooltip.outerWidth() - this.tip.outerWidth()/2;
-					topPos = bearerTop + this.bearer.outerHeight()/2 - this.tooltip.outerHeight()/2;
+					leftPos += -this.tooltip.outerWidth() - this.tip.outerWidth()/2;
+					topPos += this.bearer.outerHeight()/2 - this.tooltip.outerHeight()/2;
 					break;
 			}
 			this.tooltip.css('left', leftPos);
@@ -115,7 +125,7 @@
 
 		setEvents: function(){
 			var dt = this;
-			if(this.options.trigger == "hover" || this.options.trigger == "mouseover" || this.options.trigger == "onmouseover"){
+			if(dt.mouseOverMode){
 				this.bearer.mouseover( function(){
 					dt.setPositions();
 					dt.show();
@@ -152,27 +162,27 @@
 		},
 
 		setConfirmEvents: function(){
-			var t = this;
+			var dt = this;
 			this.tooltip.find('li.darktooltip-yes').click( function(e){
-				t.onYes();
+				dt.onYes();
 				e.stopPropagation();
 			});
 			this.tooltip.find('li.darktooltip-no').click( function(e){
-				t.onNo();
+				dt.onNo();
 				e.stopPropagation();
 			});
 		},
 
 		finalMessage: function(){
 			if(this.options.finalMessage){
-				var t = this;
-				t.tooltip.find('div:first').html(this.options.finalMessage);
-				t.tooltip.find('ul').remove();
-				t.setPositions();
+				var dt = this;
+				dt.tooltip.find('div:first').html(this.options.finalMessage);
+				dt.tooltip.find('ul').remove();
+				dt.setPositions();
 				setTimeout( function(){
-					t.hide();
-					t.setContent();
-				}, t.options.finalMessageDuration);
+					dt.hide();
+					dt.setContent();
+				}, dt.options.finalMessageDuration);
 			}else{
 				this.hide();
 			}
