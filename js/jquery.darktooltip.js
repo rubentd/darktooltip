f087ae695eb1339382de6dae750b1528748fd6ab
Ruben Torres <rubentdlh@gmail.com>
2014-08-04 17:18:16 -0400

    Add modal background

--- js/jquery.darktooltip.js
+++ js/jquery.darktooltip.js
@@ -1,5 +1,5 @@
 /* 
- * DarkTooltip v0.3.0
+ * DarkTooltip v0.3.1
  * Simple customizable tooltip with confirm option and 3d effects
  * (c)2014 Rubén Torres - rubentdlh@gmail.com
  * Released under the MIT license
