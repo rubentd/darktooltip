f0e6d973e99520f7f2127793d46adbcb7fbb7f8e
Ruben Torres <rubentdlh@gmail.com>
2016-05-15 22:46:24 -0300

    update version number

--- dist/jquery.darktooltip.js
+++ dist/jquery.darktooltip.js
@@ -1,5 +1,5 @@
 /*
- * DarkTooltip v0.4.0
+ * DarkTooltip v0.4.1
  * Simple customizable tooltip with confirm option and 3d effects
  * (c)2014 Rubén Torres - rubentdlh@gmail.com
  * Released under the MIT license
